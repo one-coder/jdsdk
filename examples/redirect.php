8d72670549cf1b04e26e99e3ae558484cca5e98d
xding <web_dxj@163.com>
2015-11-23 17:37:32 +0800

    edit some file

--- examples/redirect.php
+++ examples/redirect.php
@@ -5,8 +5,9 @@
   * 处理Oauth2回调
   *
   * 默认将access token等信息保存到配置文件中, 如果需要返回access token对象,请传入参数true
+  * Jd::redirect(true);
   *
-  * 如果处理回调错误, Jd内容则直接处理了异常
+  * 如果回调错误, Jd内部则直接处理了异常
   */
 
 require_once 'autoloader.php';
